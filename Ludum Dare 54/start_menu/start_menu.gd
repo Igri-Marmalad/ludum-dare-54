b76603df4d495cfb970e8ee59d19df2a6fe11784
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2023-10-02 00:38:09 +0200

    created main level scene

--- Ludum Dare 54/start_menu/start_menu.gd
+++ Ludum Dare 54/start_menu/start_menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 signal start_game()
 
 func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://tilemap_scene.tscn")
+	get_tree().change_scene_to_file("res://level/main_level.tscn")
 	
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
